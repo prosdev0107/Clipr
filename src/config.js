196572662c9cfe2f340bcdf782c1285483aeda1a
Nicolas Mercier <nicolaspro14@gmail.com>
2018-06-18 20:25:58 +0200

    update config

--- src/config.js
+++ src/config.js
@@ -7,15 +7,15 @@ const dev = {
 
 const staging = {
     api_clipr: {
-        API_BASE_URL: "http://app.clipr.co/fr/api",
-        TOKEN_ENDPOINT: "http://app.clipr.co/oauth/v2/token"
+        API_BASE_URL: "https://test.clipr.co/fr/api",
+        TOKEN_ENDPOINT: "https://test.clipr.co/oauth/v2/token"
     }
 }
 
 const prod = {
     api_clipr: {
-        API_BASE_URL: "http://app.clipr.co/fr/api",
-        TOKEN_ENDPOINT: "http://app.clipr.co/oauth/v2/token"
+        API_BASE_URL: "https://app.clipr.co/fr/api",
+        TOKEN_ENDPOINT: "https://app.clipr.co/oauth/v2/token"
     }
 }
 
