2b5893c39e07870b3ed8128441410fc37a5603f3
Nicolas Mercier <nicolaspro14@gmail.com>
2018-10-08 20:27:14 +0200

    Retry build

--- .ebextensions/setup/hooks/appdeploy/post/01_build_app.sh
+++ .ebextensions/setup/hooks/appdeploy/post/01_build_app.sh
@@ -4,4 +4,4 @@ set -xe
 
 npm config set scripts-prepend-node-path true
 
-REACT_APP_STAGE=staging CI=true npm run build --prefix /var/app/current
+REACT_APP_STAGE=staging CI=true npm run build --prefix /var/app/current 
