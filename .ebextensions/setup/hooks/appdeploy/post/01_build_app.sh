e9d1a1f1aa4a3a32340acdf483a0b4c33aaa9b74
Nicolas Mercier <nicolaspro14@gmail.com>
2018-10-08 21:01:18 +0200

    update config

--- .ebextensions/setup/hooks/appdeploy/post/01_build_app.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -xe
-
-#npm config set scripts-prepend-node-path true
-
-#REACT_APP_STAGE=staging CI=true npm run build --prefix /var/app/current
--- .ebextensions/setup/npm/install.sh
+++ .ebextensions/setup/npm/install.sh
@@ -11,7 +11,7 @@ then
     curl --silent --location https://rpm.nodesource.com/setup_10.x | sudo bash - &&
 
     # Install node
-    yum -y install nodejs
+    yum install -y nodejs --enablerepo=epel
 
     # Tell wkhtmltopdf where to find lib
     LD_LIBRARY_PATH=/usr/lib:/usr/lib64:/usr/local/lib
