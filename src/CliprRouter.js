786398353c01565f9593bba2695ab30ae90da3d7
Nicolas Mercier <nicolaspro14@gmail.com>
2018-10-09 21:02:40 +0200

    Change health check mode

--- src/CliprRouter.js
+++ src/CliprRouter.js
@@ -1,12 +1,10 @@
 import React from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import App from './App'
-import Health from './Health'
 
 const CliprRouter = () => (
     <Router>
         <div>
-            <Route exact path="/health/check" component={Health} />
             <Route exact path="/" component={App} />
         </div>
     </Router>
